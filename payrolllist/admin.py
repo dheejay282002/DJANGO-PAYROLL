e6aac8d72d03f3174ce44d1168b9b430509a99d8
dheejay282002 <deejay.cristobal_cyn@isu.edu.ph>
2025-10-19 20:32:11 +0800

    Delete payrolllist directory

--- payrolllist/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Payroll, Base_payroll
-# Register your models here.
-
-admin.site.register(Payroll)
-admin.site.register(Base_payroll)
--- payrolllist/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PayrolllistConfig(AppConfig):
-    name = 'payrolllist'
